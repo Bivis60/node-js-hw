8123f90e48d8c4d4342749063e4440dc6d6d3c86
Bivis60 <89808390+Bivis60@users.noreply.github.com>
2023-06-23 23:20:44 +0300

    Кінець проекту

---
+++ README.md
@@ -0,0 +1,1 @@
+https://monosnap.com/list/6495fa7e764bb94886a92fad
--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log("Hello world");
+const contacts = require("./contacts");
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.table(allContacts);
+    case "get":
+      const contact = await contacts.getContactById(id);
+      return console.log(contact);
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      return console.log(newContact);
+    case "remove":
+      const removeContact = await contacts.removeContact(id);
+      return console.log(removeContact);
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
+
+// invokeAction({ action: "list" });
+// invokeAction({ action: "get", id: "Z5sbDlS7pCzNsnAHLtDJd" });
+// invokeAction({ action: "add", name: "Reuben Henry", email: "pharetra.ut@dictum.co.uk", phone: "(715) 598-5792" });
+// invokeAction({ action: "remove", id: "Z5sbDlS7pCzNsnAHLtDJd" });
